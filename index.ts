582d8fd9f7be979940b0d9e57ca6e467d3179b51
altrios <famartinez.ti@gmail.com>
2026-06-12 12:28:07 +0200

    mcp basico

--- index.ts
+++ index.ts
@@ -1,27 +1,37 @@
 import "dotenv/config";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import express from "express";
+import cors from "cors";
+
+const port = 3000;
+const api = express();
+api.use(express.json());
+api.use(cors());
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
 
-async function main() {
+api.post("/", async (req: any, res: any) => {
   const val = await prisma.Products.create({
-     data: {
+    data: {
       title: "tablet",
       price: 15.3,
       description: "es hecha en china",
     },
   });
   console.log(val);
-}
-
-main()
-  .then(async () => {
+  try {
     await prisma.$disconnect();
-  })
-  .catch(async (e) => {
+  } catch (e) {
     console.error(e);
     await prisma.$disconnect();
     process.exit(1);
+  }
+  res.send("holi");
 });
+
+api.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
---
+++ mcp/mcp_server.ts
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+import readline from "readline";
+
+// con este mcp hacemos lectura de la consola para permitir la interaccion de la IA con la BD, quitandole dichas conexiones a la api central
+
+const directUrl = process.env.DIRECT_URL;
+// comprobamos que existe el directurl en el archivo .env
+if (!directUrl) {
+  throw new Error("DIRECT_URL is required in .env");
+}
+
+// hacemos la conexion a la BD con la libreria de prisma
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: directUrl }),
+});
+
+function send(obj: any) {
+  process.stdout.write(JSON.stringify(obj) + "\n");
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+console.error("Custom MCP stdio server: ready");
+rl.on("line", async (line) => {
+  let msg: any = null;
+  try {
+    msg = JSON.parse(line);
+  } catch (err) {
+    send({ id: null, ok: false, error: "invalid json" });
+    return;
+  }
+
+  const id = msg.id ?? null;
+  send({ id, ok: true, error: "hola tu" });
+});
